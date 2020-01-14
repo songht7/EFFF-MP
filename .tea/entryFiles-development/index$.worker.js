b2d9ea7f2667859876368c2d436337d491ed41f3
Leon Song <leon.ting@okkuaixiu.com>
2020-01-14 12:00:42 +0800

    xxxx

--- .tea/entryFiles-development/index$.worker.js
+++ .tea/entryFiles-development/index$.worker.js
@@ -36,6 +36,7 @@ require('../../node_modules/mini-antui/es/picker-item/index?hash=05d2a9730dd6009
 require('../../node_modules/mini-antui/es/flex/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../node_modules/mini-antui/es/flex/flex-item/index?hash=05d2a9730dd6009bf9446182f9c985f40f8c0f43');
 require('../../pages/index/index?hash=53f1b4e59a8ccc9e023d68e7a023595c75cc4035');
+require('../../pages/thx/thx?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 }
 self.bootstrapApp ? self.bootstrapApp({ success }) : success();
 }
